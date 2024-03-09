304b8f18173d440cc7de277595e01861ccd94584
Timm-Osorio <162040931+Timm-Osorio@users.noreply.github.com>
2024-03-10 03:07:32 +0800

    oo na mahal na kita wag mo na mang abusuhin

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,14 @@
 <script>
+  import { goto } from "$app/navigation";
+
+  let userName = "Childish Reymark";
+  
+  function gotoDashb() {
+		goto('/StudentDashboard');
+	}
+  function gotoDashR() {
+		goto('/DocumentRequest');
+	}
 </script>
 
 <header class="flex flex-col relative z-20 bg-white w-full">
@@ -16,19 +26,21 @@
         <p class="text-sm italic">Good Life Start Here!</p>
       </div>
     </div>
-    <div class="flex gap-5 justify-center items-center">
-      <label class="btn btn-circle swap swap-rotate">
-  
-        <!-- this hidden checkbox controls the state -->
-        <input type="checkbox" />
-        
-        <!-- hamburger icon -->
-        <svg class="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
-        
-        <!-- close icon -->
-        <svg class="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
-        
-      </label>
+    <div class="flex gap-2">
+      <div class="dropdown dropdown-bottom dropdown-end">
+        <div tabindex="0" role="button" class="btn p-2"><box-icon name='file' class="h-full w-full"></box-icon></div>
+        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+          <li><a on:click={gotoDashb}><box-icon name='dashboard' type='solid' ></box-icon>Dashboard</a></li>
+          <li><a on:click={gotoDashR}><box-icon name='notepad'></box-icon>Document Request</a></li>
+        </ul>
+      </div>
+      <div class="dropdown dropdown-bottom dropdown-end">
+        <div tabindex="1" role="button" class="btn p-2"><box-icon name='user-circle' class="h-full w-full"></box-icon></div>
+        <ul tabindex="1" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+          <li><a><box-icon name='user-detail' type='solid' ></box-icon>Account</a></li>
+          <li><a><box-icon name='log-out' ></box-icon>Log Out</a></li>
+        </ul>
+      </div>
     </div>
   </div>
   
