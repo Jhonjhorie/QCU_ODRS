59b7571d5b13ba03b2480bf0dc8ef834e44bd2a4
Inzagh1 <corporal.paolo.11162002@gmail.com>
2024-03-09 21:45:05 +0800

    Sidebar
    pano?

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,31 @@
+<div class=" h-full w-[300px] bg-white z-20 shadow-xl fixed " >
+    <div class="pt-7">
+        <img src="" class="w-[130px] h-[130px] rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mx-auto" alt="pao" />
+    </div>
+       
+    
+    <h1 class="text-black font-semibold text-[25px] text-center pt-2" > ADMIN </h1>
+    <h1 class="text-black font-semibold text-[15px] text-center pb-2 pt-0" > Haewon Mahal </h1>
+
+    <div class="h-1 w-[250px] bg-[#2f2f2ff0] mx-auto "> </div>
+
+    <ul class="  justify-end ">
+        <li class="pt-3 text-center">
+            <button class="h-[40px] rounded-[10px] hover:bg-slate-800 hover:scale-95 btn-wide bg-slate-900  ">DASHBOARD</button> 
+        </li>
+        <li class="pt-2 text-center">
+            <button class="h-[40px] rounded-[10px] hover:bg-slate-800 hover:scale-95 btn-wide bg-slate-900">DOCUMENTS</button>
+        </li> 
+        <li class="pt-2 text-center">
+            <button class="h-[40px] rounded-[10px] hover:bg-slate-800 hover:scale-95 btn-wide bg-slate-900">ACCOUNTS</button>
+        </li>
+        <li class="pt-2 text-center">
+            <button class="h-[40px] rounded-[10px] hover:bg-slate-800 hover:scale-95 btn-wide bg-slate-900">HISTORY</button>
+        </li> 
+       
+        <li class="fixed pl-5 pb-5 bottom-0 text-center">
+            <button  class="h-[40px] rounded-[10px] hover:bg-slate-800 hover:scale-95  btn-wide bg-slate-900">LOGOUT</button>
+        </li> 
+    </ul>
+    
+</div>
