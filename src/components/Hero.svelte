d54e40088ee994e12b2ee734184c9d6a7f599695
Timm-Osorio <162040931+Timm-Osorio@users.noreply.github.com>
2024-04-07 23:33:12 +0800

    qwe

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -8,12 +8,14 @@
 	}
 </script>
 <SectionWrapper>
-  <div class="bg-slate-100 w-full h-8"></div>
-  <div class="absolute top-2 right-10"><p class="text-md italic text-blank">Good Life Start Here!</p></div>
-  <div class="hero min-h-[95vh] bg-cover" style="background-image: url('QCU CAMPUS.jpg');">
+  <main class=" flex flex-col">
+  <div class="absolute top-24 right-20 bg-slate-100 bg-opacity-30 p-2 rounded-lg"><p class="text-md italic text-white">Good Life Start Here!</p></div>
+  <div class="absolute top-56 right-20 bg-slate-100 bg-opacity-30 p-2 rounded-lg"><p class="text-md italic text-white">#QCU1</p></div>
+  <div class="absolute top-40 right-20 bg-slate-100 bg-opacity-30 p-2 rounded-lg"><p class="text-md italic text-white">#1 Local University of Employable Graduates</p></div>
+  <div class="hero h-[97vh] bg-cover" style="background-image: url('QCU CAMPUS.jpg');">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content gap-10">
-      <img src="QCULOGO.png" class=" w-64 rounded-lg shadow-3xl p-5 bg-white bg-opacity-50" alt="" />
+      <img src="QCULOGO.png" class=" w-64 rounded-lg shadow-3xl p-5 bg-white bg-opacity-30" alt="" />
       <div class="text-left max-w-xl">
         <h1 class="mb-5 text-5xl font-bold">I LOVE YOU MAAM MACEH</h1>
         <p class="mb-5">For Students and Graduates of QCU San Bartolomeo (Main) <br />
@@ -23,4 +25,5 @@
       </div>
     </div>
   </div>
+</main>
 </SectionWrapper>
