eafba20f0693ddaf9cf194ad1f06bbb8f5c43808
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2024-04-14 22:51:51 +0800

    Search

--- src/routes/Registrar/Requests/+page.svelte
+++ src/routes/Registrar/Requests/+page.svelte
@@ -1,140 +1,42 @@
 <script>
-  // @ts-nocheck
+  // Import necessary components
   import SectionWrapper from "../../../components/SectionWrapper.svelte";
   import Header from "../../../components/Registrar/RegistrarHeader.svelte";
   import Sidebar from "../../../components/Registrar/RegistrarSidebar.svelte";
-  import { goto } from "$app/navigation";
-  import { db } from "$lib/firebase/firebase";
-  import { collection, query, where, getDocs, doc, updateDoc, orderBy } from 'firebase/firestore';
+  import Search from "../../../components/Registrar/RegistrarSearch.svelte";
+  import Requests from "../../../components/Registrar/RegistrarRequests.svelte";
+  import { writable } from 'svelte/store';
 
-  let requests = [];
-  let loading = true;
-  let searchText = '';
-
-  async function fetchRequests() {
-    const q = query(
-      collection(db, 'docRequests'),
-      where('dept_Title', '==', 'Bachelor Of Science In Computer Science'),
-      where('status', '!=', 2),
-      orderBy('date_Req', 'desc')
-    );
-
-    try {
-      const querySnapshot = await getDocs(q);
-      requests = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    } catch (error) {
-      console.error('Error fetching requests:', error);
-    } finally {
-      loading = false;
-    }
-  }
-
-  fetchRequests();
-
-  function gotoInfo(id) {
-    goto(`/Registrar/RequestInfo?id=${id}`);
-  }
-
-  function search() {
-    return requests.filter(request =>
-      request.student_Num.toLowerCase().includes(searchText.toLowerCase())
-    );
-  }
-
-  function resetSearch() {
-    searchText = '';
-    search();
+  let textsdas = 'Search';
+  let isSearchVisible = writable(true); 
+  function toggleVisibility() {
+    isSearchVisible.update(value => !value);
+    textsdas = 'Back'
   }
 </script>
+
 <main class="flex flex-col">
   <SectionWrapper>
     <Header />
     <div class="flex flex-row gap-5 mx-auto w-full">
-      <Sidebar />
-    
+      <Sidebar />      
+
       <div class="flex flex-col flex-1 mx-auto w-full">
         <div class="flex justify-between">
           <h1 class="p-3 text-3xl text-black font-bold">REQUESTS</h1>
           <div class="flex mr-4 items-center gap-2">
-            <h2 class="text-3l text-black">Search:</h2>
-            <input type="text" class="p-2 my-2 rounded-md border border-gray-400" placeholder="Input Student ID..." bind:value={searchText} />
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={search}>Search</button>
-            <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" on:click={resetSearch}>Reset</button>
+            <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" on:click={toggleVisibility}>{textsdas} --></button>
           </div>
         </div>
+
+        {#if $isSearchVisible}
         
-        {#if loading}
-          <div class="flex justify-center items-center h-[70vh]">
-            <span class="h-14 w-10 loading loading-dots loading-md "></span>
-          </div>
+        <Requests />
         {:else}
-          <div class="h-[70vh] w-auto p-2 bg-slate-100 rounded-md shadow-lg mr-4">
-            <div class="overflow-x-auto w-auto h-[67.5vh] bg-slate-200">
-              <table class="table table-xs">
-                <!-- head -->
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Transaction Number</th>
-                    <th>Student Name</th>
-                    <th>Student No.</th>
-                    <th>Document Requested</th>
-                    <th>Date</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <!-- body -->
-                <tbody>
-                  {#each search() as request, index}
-                      <tr class="hover:bg-gray-400" on:click={() => gotoInfo(request.id)}>
-                      <th>{index + 1}</th>
-                      <td>{request.id}</td>
-                      <td>{request.student_Name}</td>
-                      <td>{request.student_Num}</td>
-                      <td>{request.doc_ID}</td>
-                      <td>{request.date_Req.toDate().toLocaleDateString('en-US')}</td>
-                      <td>
-                        {#if request.status == 2}
-                          <ul class="steps w-full">
-                              <li class="step step-success"></li>
-                              <li class="step step-success"></li>
-                              <li class="step step-success font-bold">Completed</li>
-                          </ul>
-                        {:else if request.status == 1}
-                          <ul class="steps w-full">
-                              <li class="step step-warning"></li>
-                              <li class="step step-warning font-bold">Claiming</li>
-                              <li class="step"></li>
-                          </ul>
-                        {:else}
-                          <ul class="steps w-full">
-                              <li class="step step-info font-bold">Pending</li>
-                              <li class="step"></li>
-                              <li class="step"></li>
-                          </ul>
-                        {/if}
-                      </td>
-                    </tr>
-                  {/each}
-                </tbody>
-              </table>
-            </div>
-          </div>
+        <Search />
         {/if}
       </div>
+
     </div>
   </SectionWrapper>
 </main>
-
-<style>
-  th {
-    font-weight: bold;
-    font-size: 15px;
-    padding: 10px;
-    color: black;
-  }
-
-  td {
-    color: black;
-  }
-</style>
